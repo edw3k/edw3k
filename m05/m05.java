4a4244bf42684be3ac4eea9a41e5ecb101c71e54
guifr <guifreagustin2002@gmail.com>
2022-01-21 09:42:59 +0100

    COMMIT INICIAL

---
+++ m05/m05.java
@@ -0,0 +1,12 @@
+package m05;
+
+import java.util.ArrayList;
+
+public class m05 {
+    public static void main(String[] args) {
+        ArrayList<String> unga=new ArrayList<>();
+        unga.add("hello ");
+        unga.add("World");
+        System.out.println(unga);
+    }
+}
